620e0c1b5c40e6e25a39f30ef54459395f1842d9
bchen41 <bettychen41@outlook.com>
2021-11-12 13:05:05 -0500

    Created and looped through HTML tags and text to create rows

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function displayCurrentDay() {
 }
 
 setInterval(displayCurrentDay, 1000);
+
+// Created elements with addClass to append to divContainer by calling function to generate rows through a for loop
+var divContainerEl = $(".container");
+function generateRows(iterations) {
+  if (!iterations) {
+    iterations = 1;
+  }
+
+  for (var i = 0; i < iterations; i++) {
+    // Created array to loop the standard business hours with either AM/PM displayed in time block
+    var businessHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
+    var spanHours = ["AM", "AM", "AM", "PM", "PM", "PM", "PM", "PM", "PM"];
+
+    var sectionRowsEl = $("<section>").addClass("row time-block");
+
+    var divHoursEl = $("<div>").addClass("col-2 hour").text(businessHours[i]);
+    var spanHoursEl = $("<span>").text(spanHours[i]);
+    var timeHoursBlock = divHoursEl.append(spanHoursEl);
+
+    var textAreasEl = $("<textarea>").addClass("col-9 description");
+
+    var saveButtonsEl = $("<button>").addClass("col-1 saveBtn");
+    var iconSaveBtnEl = $("<i>").text("SAVE");
+    var saveButton = saveButtonsEl.append(iconSaveBtnEl);
+
+    sectionRowsEl.append(timeHoursBlock, textAreasEl, saveButton);
+    divContainerEl.append(sectionRowsEl);
+  }
+}
+
+generateRows(9);
