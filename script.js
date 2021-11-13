60e3e70aed95a600c6123051dec454159deb69ac
bchen41 <bettychen41@outlook.com>
2021-11-12 23:08:45 -0500

    used different method to  apply css colors to textareas

--- script.js
+++ script.js
@@ -1,85 +1,69 @@
-$(document).ready(() => {
-  var currentDayEl = $("#currentDay");
-  var currentHour = parseInt(moment().format("h"));
-  var saveButtonsEl = $("<button>").addClass("col-1 saveBtn");
+var currentDayEl = $("#currentDay");
+var saveButtonsEl = $("<button>").addClass("col-1 saveBtn");
 
-  // Displayed current day and time
-  function displayCurrentDay() {
-    var rightNow = moment().format("dddd, MMMM Do, YYYY [at] LTS");
-    currentDayEl.text(rightNow);
+// Displayed current day and time
+function displayCurrentDay() {
+  var rightNow = moment().format("dddd, MMMM Do, YYYY [at] LTS");
+  currentDayEl.text(rightNow);
+}
+
+setInterval(displayCurrentDay, 1000);
+
+// Created elements with addClass to append to divContainer by calling function to generate rows through a for loop
+var divContainerEl = $(".container");
+function generateRows(iterations) {
+  if (!iterations) {
+    iterations = 1;
   }
 
-  setInterval(displayCurrentDay, 1000);
+  for (var i = 0; i < iterations; i++) {
+    var businessHours = moment()
+      .hour(9 + i)
+      .minute(0);
+    var timeBlockText = businessHours.format("LT");
 
-  // Created elements with addClass to append to divContainer by calling function to generate rows through a for loop
-  var divContainerEl = $(".container");
-  function generateRows(iterations) {
-    if (!iterations) {
-      iterations = 1;
+    var sectionRowsEl = $("<section>").addClass("row time-block");
+
+    var spanHoursEl = $("<span>").text(timeBlockText);
+    var divHoursEl = $("<div>").addClass("col-2 hour");
+    var timeHoursBlock = divHoursEl.append(spanHoursEl);
+
+    var textAreasEl = $("<textarea>")
+      .addClass("col-9 description")
+      .attr("id", timeBlockText);
+
+    var saveButtonsEl = $("<button>").addClass("col-1 saveBtn");
+
+    var iconSaveBtnEl = $("<i>").text("SAVE");
+    var saveButton = saveButtonsEl.append(iconSaveBtnEl);
+
+    sectionRowsEl.append(timeHoursBlock, textAreasEl, saveButton);
+    divContainerEl.append(sectionRowsEl);
+
+    var currentHour = moment();
+    if (businessHours.isBefore(currentHour.minute(0))) {
+      // set class for before
+      textAreasEl.addClass("past");
+    } else if (businessHours.isSame(currentHour.minute(0))) {
+      // set some color
+      textAreasEl.addClass("present");
+    } else {
+      // set color for after
+      textAreasEl.addClass("future");
     }
 
-    for (var i = 0; i < iterations; i++) {
-      // Created array to loop the standard business hours with either AM/PM displayed in time block
-      var businessHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-      var spanHours = ["AM", "AM", "AM", "PM", "PM", "PM", "PM", "PM", "PM"];
+    saveButtonsEl.on("click", setToDos);
+    console.log(saveButtonsEl);
 
-      var sectionRowsEl = $("<section>").addClass("row time-block");
-
-      var divHoursEl = $("<div>").addClass("col-2 hour").text(businessHours[i]);
-      var spanHoursEl = $("<span>").text(spanHours[i]);
-      var timeHoursBlock = divHoursEl.append(spanHoursEl);
-
-      var textAreasEl = $("<textarea>").addClass("col-9 description");
-
-      var saveButtonsEl = $("<button>").addClass("col-1 saveBtn");
-
-      var iconSaveBtnEl = $("<i>").text("SAVE");
-      var saveButton = saveButtonsEl.append(iconSaveBtnEl);
-
-      sectionRowsEl.append(timeHoursBlock, textAreasEl, saveButton);
-      divContainerEl.append(sectionRowsEl);
-
-      if (
-        divHoursEl.immediateText() < currentHour ||
-        spanHoursEl.immediateText() == "AM"
-      ) {
-        textAreasEl.addClass("past");
-      } else if (divHoursEl.immediateText() == currentHour) {
-        textAreasEl.addClass("present");
+    function setToDos() {
+      var textAreasToDo = textAreasEl.value;
+      if (textAreasToDo === undefined) {
+        alert("Nothing to save");
       } else {
-        textAreasEl.addClass("future");
       }
-      console.log(parseInt(divHoursEl.immediateText(i)));
-      console.log(currentHour);
-      console.log(parseInt(divHoursEl.immediateText(i)) == currentHour);
     }
   }
+}
 
-  // https://stackoverflow.com/questions/3442394/using-text-to-retrieve-only-text-not-nested-in-child-tags
-  // Used this function to grab the specific text inside divHoursEl without grabbing the text inside span, which is the child of div
-  $.fn.immediateText = function () {
-    return this.contents().not(this.children()).text();
-  };
-
-  generateRows(9);
-});
+generateRows(9);
 // Added classes to textAreasEl to change color signifying past, present or future
-
-// saveButtonsEl[0].addEventListener("click", setToDos);
-
-// function setToDos(event) {
-//   var element = event.target;
-//   if (element.matches(saveButtonsEl[0])) {
-//     var textAreasToDo = textAreasEl.value;
-
-//     if (textAreasToDo === "") {
-//       alert("Nothing to save");
-//       console.log("test");
-//     }
-//   }
-
-//   //   saveButtonsEl[0].addEventListener("click", setToDos);
-//   //   $('.saveBtn').each(()=>{
-//   //       $()
-//   //   })
-// }
